39561e2902af1a0c2fe477682381252a1d1a22be
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-13 21:23:05 +0200

    section 15. (functions file)

--- 1st_app_todos.py
+++ 1st_app_todos.py
@@ -1,19 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of to-do items"""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-# print(help(get_todos))
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items list in the text file. """
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
-# print(help(write_todos))
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
