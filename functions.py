971da3126f6c207d9c14250bd195fe7f221c1b32
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-14 20:55:25 +0200

    section 18. (gui)

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of to-do items. """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
@@ -7,10 +10,11 @@ def get_todos(filepath="todos.txt"):
 # print(help(get_todos))
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
 # print(help(write_todos))
+
 
