4b95a77913de83f243a7e12cc99bda9292202434
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-16 19:42:56 +0200

    bonus 3

---
+++ bonus/bonus3.py
@@ -0,0 +1,13 @@
+meals = ["pasta", "pizza", "salad"]
+
+for meal in meals:
+    print(meal.capitalize())
+
+for meal in "meals":
+    print(meal.capitalize())
+
+print("Bye")
+
+""" The basic difference between While loop and For loop is that: 
+ a While loop continues to work as long as condition is true 
+ a For loop continues to work as long as there are items inside the list """
