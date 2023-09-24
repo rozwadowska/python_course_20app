a1a0e952ee412330f369264bac4f613b727cd4c4
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-24 17:58:25 +0200

    ex25.1

---
+++ exercise/ex26.2.py
@@ -0,0 +1,13 @@
+""" Extend the given code so the code prints out
+a list containing the number of characters
+for each username.
+
+The output of your code should be as below:
+
+    [9, 11, 11] """
+
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+
+length = [len(username) for username in usernames]
+
+print(length)
