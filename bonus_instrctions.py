533cdbe809fb421d8b018eaad053e9d9448fc54f
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-25 14:19:49 +0200

    bonus 9 + instruction

--- bonus_instrctions.py
+++
@@ -1,4 +0,0 @@
-""" For now this is an empty file,
-will be updated later.
-
-* add information about helpful activities in the Terminal """
---
+++ bonus_instructions.py
@@ -0,0 +1,24 @@
+""" For now this is an empty file,
+will be updated later.
+
+* add information about helpful activities in the Console
+
+dir() method returns a list of valid attributes of
+the specified object. If no argument passed,
+it returns the list of names in the current local scope.
+
+dir(object)
+e.g. dir(list)
+
+help() function invokes the interactive built-in
+help system. If the argument is a string, then
+the string is treated as the name of a module,
+function, class, keyword, or documentation topic,
+and a help page is printed on the console.
+If the argument is any other kind of object,
+a help page on the object is displayed.
+
+help(object)
+e.g. help(print)
+
+  """
