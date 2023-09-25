cae76f09c1857009f3e24a7d6ad7796128a73a74
Wiktoria <wiktoria.rozwadowska.wr@gmail.com>
2023-09-25 16:40:07 +0200

    ex29.1

---
+++ exercise/ex29.1.py
@@ -0,0 +1,17 @@
+""" Write a program that:
+
+1. asks users to enter a password.
+
+2. returns "Great password there!" if the password
+ has more than 7 characters.
+
+3. returns "Your password is weak." if the password has 7
+or fewer characters. """
+
+
+password = input("Enter a new password: ")
+
+if len(password) > 7:
+    print("Great password there!")
+elif len(password) <= 7:
+    print("Your password is weak.")
